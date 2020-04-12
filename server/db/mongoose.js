a0cd98cf3b2bf47e4fe7296b271ed65b457b8c94
rahul-abhishek-au6 <rahulabhishek2june@gmail.com>
2020-04-12 17:08:17 +0530

    Add

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+mongoose.connect("mongodb://127.0.0.1:27017/HMSAttainU");
 
 module.exports = {mongoose};
