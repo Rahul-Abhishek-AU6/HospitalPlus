c76cfea05cc66b617df36c3f4e8cad1475dc8600
Rahul-Abhishek-AU6 <58417497+Rahul-Abhishek-AU6@users.noreply.github.com>
2020-04-19 14:00:31 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+mongoose.connect("mongodb://root:qwerty123@ds019048.mlab.com:19048/e-commerce-course-project");
 
 module.exports = {mongoose};
